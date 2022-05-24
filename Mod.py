59bcfeba522c1315322778d40fa9934e5a84e874
Valeria Todaro <80830401+ValeriaTodaro@users.noreply.github.com>
2022-05-24 18:11:32 +0200

    /GenESMDA

---
+++ Mod.py
@@ -0,0 +1,17 @@
+#Mod.py is the module that links GenES-MDA with the forward model
+#It must be adapted according to each specific application
+
+#Import here the required Python packages and libraries
+
+'''
+def write_input(par):
+    #Function that writes the parameters into their proper location in the input files of the forward model.
+    
+def run():
+    #Function that run the forward model in batch mode.
+
+      
+def read_output():
+    #Function that reads the outputs of the forward model corresponding to the observation data.
+    return values
+'''
