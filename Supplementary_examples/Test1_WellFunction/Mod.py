218b0fb3ca63f66c85bf244ec2c08c8f6862c9ea
Valeria Todaro <80830401+ValeriaTodaro@users.noreply.github.com>
2025-04-27 19:05:20 +0200

    Add files via upload

--- Supplementary_examples/Test1_WellFunction/Mod.py
+++ Supplementary_examples/Test1_WellFunction/Mod.py
@@ -6,19 +6,26 @@ def write_input(par):
         np.savetxt(file, par, fmt='%.4e', newline='\n') 
       
 def run():
+    #Read parameters
     par=np.loadtxt('par.txt', dtype=float)
     T=par[0] # Transmissivity in m²/s
     S=par[1] # Storativity (dimensionless)
     
+    #Read observation times
+    os.chdir('..')
+    obs=np.loadtxt('Obs.txt')
+    os.chdir('Model')
+    t=obs[:,3]
+    
+    #Run forward model
     from Forward_model import well_function
-    well_function(T, S)     
+    s=well_function(T, S, t)  
+    
+    #Write response variable at obsevation locations
+    with open('pred.txt','wb') as file:
+        data = np.column_stack((t, s))
+        np.savetxt(file, data, fmt=['%.0f','%.3e'], newline='\n') 
    
 def read_output():
-    os.chdir('..')
-    Obs_file=np.loadtxt('Obs.txt')
-    os.chdir('Model')
-    t_obs=Obs_file[:,3]
-    t,s=np.loadtxt('pred.txt', dtype=float, unpack=True)
-    idx=np.where(np.isin(t, t_obs))[0]
-    pred=s[idx]
+    _, pred =np.loadtxt('pred.txt', dtype=float, unpack=True)
     return pred
